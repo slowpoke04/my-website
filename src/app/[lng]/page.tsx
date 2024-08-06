d7b2ef91f336b25a525615171e802f3cce88d301
Léo Petit <leo.petit@live.fr>
2024-08-06 11:27:37 +0200

    feat: few changes

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -1,26 +1,36 @@
 import About from '@/components/about'
+import ActiveSectionSetter from '@/components/client/active-section-setter'
 import Educations from '@/components/education'
 import Experiences from '@/components/experiences'
 import Header from '@/components/header'
 import Links from '@/components/links'
 import {ELanguages} from '@/types/enums'
-import Image from 'next/image'
-import {useRef, useState} from 'react'
 
-export default function Home({params}: {params: {lng: ELanguages}}) {
+export default function Main({params}: {params: {lng: ELanguages}}) {
   const {lng} = params
   return (
-    <main className='no-scrollbar flex h-full max-w-[1224px] flex-col items-start gap-12 overflow-y-auto tracking-wide md:flex-row'>
-      <Header
-        className='p-10 pb-0 sm:p-10 sm:pb-0 md:sticky md:left-0 md:top-0 md:h-full md:w-1/2 md:gap-6 md:p-20 md:pr-0 lg:w-1/2'
-        lng={lng}
-      />
+    <main className='flex w-screen flex-col items-center'>
+      <div className='flex w-full max-w-[1224px] flex-col items-start gap-12 tracking-wide md:flex-row'>
+        <Header
+          className='w-full p-6 pb-0 sm:p-10 sm:pb-0 md:sticky md:top-0 md:h-screen md:w-1/3 md:gap-6 md:p-20 md:pr-0'
+          lng={lng}
+        />
 
-      <div className='md:-pt-4 no-scrollbar flex grow flex-col items-stretch gap-16 p-10 pt-0 align-middle sm:p-10 sm:pt-0 md:-mt-16 md:h-full md:p-20 md:pl-0'>
-        <Links className='pb-4 md:invisible md:h-0 md:p-0' lng={lng} />
-        <About lng={lng} />
-        <Experiences lng={lng} />
-        <Educations lng={lng} />
+        <div
+          id='content'
+          className='flex flex-col gap-16 p-6 pt-0 align-middle sm:p-10 sm:pt-0 md:-mt-16 md:h-full md:w-2/3 md:p-20 md:pl-0'>
+          <Links className='pb-4 md:invisible md:h-0 md:p-0' lng={lng} />
+          <ActiveSectionSetter section='about'>
+            <About lng={lng} />
+          </ActiveSectionSetter>
+          <ActiveSectionSetter section='experience'>
+            <Experiences lng={lng} />
+          </ActiveSectionSetter>
+          <ActiveSectionSetter section='education'>
+            <Educations lng={lng} />
+          </ActiveSectionSetter>
+          <div className='h-40'></div>
+        </div>
       </div>
     </main>
   )
--- src/components/education.tsx
+++ src/components/education.tsx
@@ -1,6 +1,7 @@
 import {getTranslation} from '@/lib/i18n'
 import {ELanguages} from '@/types/enums'
-import {Trans} from 'react-i18next/TransWithoutContext'
+import Tag from './tag'
+import Image from 'next/image'
 
 interface IEducationKeys {
   id?: number | string
@@ -10,9 +11,15 @@ interface IEducationKeys {
   tags: string
   description: string
   description_list: string
+  img_width: number
+  img_height: number
 }
 
-function getEducationKeys(prefix: string): IEducationKeys {
+function getEducationKeys(
+  prefix: string,
+  width: number,
+  height: number,
+): IEducationKeys {
   return {
     id: prefix,
     institution: `${prefix}_institution`,
@@ -21,28 +28,55 @@ function getEducationKeys(prefix: string): IEducationKeys {
     tags: `${prefix}_tags`,
     description: `${prefix}_description`,
     description_list: `${prefix}_description_list`,
+    img_width: width,
+    img_height: height,
   }
 }
 
-const educations: IEducationKeys[] = [
-  getEducationKeys('ens'),
-  getEducationKeys('ipgp'),
-  // getEducationKeys('sorbonne'),
-  // getEducationKeys('dijon'),
+const educationNames = ['ipgp', 'ens', 'sorbonne', 'dijon']
+const educationWidths = [672, 317, 354, 672]
+const educationHeights = [255, 159, 142, 255]
+const educations: IEducationKeys[] = educationNames.map((name, id) =>
+  getEducationKeys(name, educationWidths[id], educationHeights[id]),
+)
+
+const otherCoursesKeys = [
+  'python',
+  'data_sciences',
+  'deep_learning',
+  'inverted_pedagogy',
 ]
 
 export default async function Educations({lng}: {lng: ELanguages}) {
   const {t} = await getTranslation(lng, undefined, {keyPrefix: 'education'})
   return (
-    <section className='flex flex-col gap-12 md:-mt-4'>
+    <section id='education' className='flex flex-col gap-12 md:-mt-4'>
       <h2 className='text-sm font-semibold text-slate-900 dark:text-slate-100 md:invisible md:h-0'>
         {t('section_title')}
       </h2>
-      <ul className='flex flex-col gap-6'>
-        {educations.map(expKeys => (
-          <Education key={expKeys.id} lng={lng} educationKeys={expKeys} />
-        ))}
-      </ul>
+      <div className='flex flex-col gap-2 p-2'>
+        {/* <h3 className='text-sm font-semibold text-slate-900 dark:text-slate-100'>
+          {t('higher_education_title')}
+        </h3> */}
+        <ul className='flex flex-col gap-4'>
+          {educations.map(expKeys => (
+            <Education key={expKeys.id} lng={lng} educationKeys={expKeys} />
+          ))}
+        </ul>
+        {/* <h3 className='text-sm font-semibold text-slate-900 dark:text-slate-100'>
+          {t('other_courses_title')}
+        </h3>
+        <ul className='flex flex-col gap-4'>
+          {otherCoursesKeys.map(courseKey => (
+            <li key={courseKey} className='flex items-center gap-2'>
+              <span className='inline-block text-xl text-slate-500 dark:text-slate-300'>
+                ·
+              </span>{' '}
+              {t(courseKey)}
+            </li>
+          ))}
+        </ul> */}
+      </div>
     </section>
   )
 }
@@ -60,61 +94,33 @@ async function Education({
     returnObjects: true,
   }) as string[]
 
-  console.log('descriptionListElements', descriptionListElements)
   const tagsText = t(educationKeys.tags, {returnObjects: true}) as string[]
+  const imgPath = `/images/${educationKeys.id}.png`
 
-  const emphTextColor = 'font-semibold text-teal-700 dark:text-slate-100'
-
+  console.log(`${educationKeys.id}.png =`, `${educationKeys.id}.png`)
   return (
-    <div className='group flex flex-col gap-1 rounded p-2 sm:grid sm:grid-cols-3 md:border md:border-transparent md:hover:z-50 md:hover:border md:hover:border-slate-300 md:hover:bg-slate-100 md:dark:border-transparent md:dark:hover:bg-slate-800'>
-      <div className='text-sm font-medium text-slate-600 dark:text-slate-500 sm:col-span-1 sm:mt-[6px]'>
+    <div className='group flex flex-col items-stretch gap-2 rounded p-2 align-top sm:grid sm:grid-cols-3 md:border md:border-transparent md:hover:z-10 md:hover:border md:hover:border-slate-300 md:hover:bg-slate-100 md:dark:border-transparent md:dark:hover:border md:dark:hover:border-slate-700 md:dark:hover:bg-slate-900'>
+      <div className='mt-1 text-sm font-medium text-slate-600 dark:text-slate-500 sm:col-span-1'>
         {t(educationKeys.period)}
       </div>
-      <div className='flex flex-col gap-4 sm:col-span-2'>
-        <div>
-          <h2 className='inline-block text-xl font-medium text-slate-600 dark:text-slate-100'>
-            {t(educationKeys.degree)}
-          </h2>{' '}
-          <span className='inline-block text-slate-500 dark:text-slate-400'>
-            ·
-          </span>{' '}
-          <h3 className='inline-block text-lg font-semibold text-slate-500 dark:text-slate-400'>
-            {t(educationKeys.institution)}
-          </h3>
-        </div>
-        <p className='text-sm font-extralight text-slate-500 dark:text-slate-400'>
-          <Trans
-            t={t}
-            i18nKey={educationKeys.description}
-            components={{
-              emph: <strong className={emphTextColor} />,
-            }}
-          />
-          {/* {t(educationKeys.description)} */}
-        </p>
-        <ul className='flex flex-col gap-y-2'>
-          {descriptionListElements.map((description, idx) => (
-            <li
-              key={idx}
-              className='text-xs font-light text-slate-500 dark:text-slate-500'>
-              - {description}
-            </li>
-          ))}
-        </ul>
-        <div className='flex flex-wrap gap-2'>
+      {/* <div className='flex flex-col gap-4 sm:col-span-2'> */}
+      <div className='sm:col-span-2'>
+        <h2 className='text-md inline-block font-medium text-slate-600 dark:text-slate-300'>
+          {t(educationKeys.degree)}
+        </h2>{' '}
+        <span className='text-md inline-block text-slate-600 dark:text-slate-400'>
+          ·
+        </span>{' '}
+        <h3 className='inline-block text-sm font-light text-slate-400 dark:text-slate-400'>
+          {t(educationKeys.institution)}
+        </h3>
+      </div>
+      {/* <div className='flex flex-wrap gap-2'>
           {tagsText.map(tag => (
             <Tag key={tag} text={tag} />
           ))}
-        </div>
-      </div>
-    </div>
-  )
-}
-
-async function Tag({text}: {text: string}) {
-  return (
-    <div className='rounded-full border-none border-teal-300 px-2 py-1 text-xs font-light text-teal-600 dark:bg-teal-950 dark:text-teal-400 group-hover:dark:bg-teal-900'>
-      {text}
+        </div> */}
+      {/* </div> */}
     </div>
   )
 }
